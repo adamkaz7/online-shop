94ebb8847c58b897a5c9d551bf22eddddec33003
adamkaz7 <adamkazmierczak64@gmail.com>
2026-07-21 10:13:53 +0200

    Task 3 - Implement cart and cart item

---
+++ src/main/java/pl/adam/onlineshop/cart/Cart.java
@@ -0,0 +1,63 @@
+package pl.adam.onlineshop.cart;
+
+import lombok.NonNull;
+import pl.adam.onlineshop.product.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Cart {
+    private final List<CartItem> items = new ArrayList<>();
+
+    public Optional<CartItem> findItemByProductId(@NonNull String productId) {
+        return items.stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst();
+    }
+
+    public void addProduct(@NonNull Product product, int quantity) {
+        Optional<CartItem> existingItem = findItemByProductId(product.getId());
+        if (existingItem.isPresent()) {
+            existingItem.get().increaseQuantity(quantity);
+            return;
+        }
+        CartItem item = new CartItem(product, quantity);
+        items.add(item);
+    }
+
+    public void removeProduct(@NonNull String productId) {
+        CartItem item = findItemByProductId(productId)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Product with id " + productId + " not found in cart"
+                ));
+
+        items.remove(item);
+    }
+
+    public void changeQuantity(@NonNull String productId, int quantity) {
+        CartItem item = findItemByProductId(productId)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Product with id " + productId + " not found in cart"
+                ));
+        item.changeQuantity(quantity);
+    }
+
+    public int getTotalQuantity() {
+        return items.stream()
+                .mapToInt(CartItem::getQuantity)
+                .sum();
+    }
+
+    public List<CartItem> getItems() {
+        return List.copyOf(items);
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+}
