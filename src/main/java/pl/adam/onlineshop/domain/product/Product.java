78da375676595980297233dd19f6d4874947c703
adamkaz7 <adamkazmierczak64@gmail.com>
2026-07-24 19:41:25 +0200

    refactor: replace product setters with domain methods

--- src/main/java/pl/adam/onlineshop/domain/product/Product.java
+++ src/main/java/pl/adam/onlineshop/domain/product/Product.java
@@ -1,29 +1,66 @@
 package pl.adam.onlineshop.domain.product;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public abstract class Product {
     @NonNull
     private final UUID id;
 
-    @Setter
     @NonNull
     private String name;
 
-    @Setter
     @NonNull
     private BigDecimal price;
-
-    @Setter
     private int availableQuantity;
+
+    protected Product(@NonNull UUID id, @NonNull String name, @NonNull BigDecimal price, int availableQuantity) {
+        validateName(name);
+        validatePrice(price);
+        validateAvailableQuantity(availableQuantity);
+
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.availableQuantity = availableQuantity;
+    }
+
+    public void changeName(@NonNull String newName) {
+        validateName(newName);
+        this.name = newName;
+    }
+
+    public void changePrice(@NonNull BigDecimal newPrice) {
+        validatePrice(newPrice);
+        this.price = newPrice;
+    }
+
+    public void changeAvailableQuantity(int newQuantity) {
+        validateAvailableQuantity(newQuantity);
+        this.availableQuantity = newQuantity;
+    }
+
+    private static void validateAvailableQuantity(int availableQuantity) {
+        if (availableQuantity < 0) {
+            throw new IllegalArgumentException("Available quantity cannot be negative");
+        }
+    }
+
+    private static void validatePrice(BigDecimal price) {
+        if (price.signum() < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+    }
+
+    private static void validateName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be blank");
+        }
+    }
 
     @Override
     public String toString() {
